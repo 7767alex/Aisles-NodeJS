24ac89f23d236108dbb8ae06238a4b135e395851
Alejandro Orozco <7767alex@maill.fresnostate.edu>
2019-09-17 13:11:12 -0700

    demo
    djoefijlfkajkld

---
+++ Project/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Project
+//
+//  Created by Alex Orozco on 9/10/19.
+//  Copyright © 2019 Alex Orozco. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout <<" hello world " << endl;
+    
+    return 0;
+}
